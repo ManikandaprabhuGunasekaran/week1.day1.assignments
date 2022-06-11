e6c26a10bd0eae3bbf14419b9cd515784912915d
ManikandaprabhuGunasekaran <41508446+ManikandaprabhuGunasekaran@users.noreply.github.com>
2022-06-11 20:57:07 +0530

    Add files via upload
    Factorial

---
+++ Factorial.java
@@ -0,0 +1,18 @@
+package week1.day1.assignments;
+
+public class Factorial {
+
+	public static void main(String[] args) {
+
+		int x = 5;
+		int sum = 0;
+
+		for (int i = 1; i <= x; i++) {
+			sum = sum + x;
+
+			System.out.println(sum);
+		}
+
+	}
+
+}
